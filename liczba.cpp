1fa5a3ad343d3f8b355f4061daa13fb66c6633d9
irekpolak <irekpolak@mat.umk.pl>
2025-11-27 00:31:31 +0100

    Dodano funkcje czy_doskonala

--- liczba.cpp
+++ liczba.cpp
@@ -1,9 +1,26 @@
 #include <iostream>
 using namespace std;
 
+bool czy_doskonala(int n) {
+    if (n <= 1) return false;
+    int suma = 1;
+    for (int i = 2; i * i <= n; i++) {
+        if (n % i == 0) {
+            suma += i;
+            if (i != n / i) suma += n / i;
+        }
+    }
+    return suma == n;
+}
+
 int main() {
     int n;
     cin >> n;
-    cout << n << endl;
+    cout << n;
+    if (czy_doskonala(n))
+        cout << " jest liczba doskonala";
+    else
+        cout << " nie jest liczba doskonala";
+    cout << endl;
     return 0;
 }
