bbd7966ca13016d59f62346c28af506ace369d33
irekpolak <irekpolak@mat.umk.pl>
2026-02-04 16:58:02 +0100

    Dodano walidację dodatniej liczby naturalnej

--- liczba.cpp
+++ liczba.cpp
@@ -4,6 +4,18 @@ using namespace std;
 int main() {
     int liczba;
     cin >> liczba;
-    cout << liczba << endl;
+
+    if (liczba <= 0) {
+        cout << "B³¹d: Podaj dodatni¹ liczbê naturaln¹!" << endl;
+        return 1;
+    }
+
+    cout << liczba;
+    if (czy_doskonala(liczba)) {
+        cout << " jest liczba doskona³a" << endl;
+    } else {
+        cout << " nie jest liczba doskona³a" << endl;
+    }
     return 0;
 }
+
